b697ab536e964a94330278fdc4decbcfb0578529
Irene Teinemaa <hpc_irheta@juur.grid.eenet.ee>
2017-05-05 13:48:49 +0300

    State-based bucketing

---
+++ bucketers/StateBasedBucketer.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+from time import time
+import sys
+
+class StateBasedBucketer():
+    
+    def __init__(self):
+        self.dt_states = None
+        self.n_states = 0
+        
+    
+    def fit(self, X, y=None):
+        
+        self.dt_states = X.drop_duplicates()
+        self.dt_states = self.dt_states.assign(state = range(len(self.dt_states)))
+        
+        self.n_states = len(self.dt_states)
+        
+        return self
+    
+    
+    def predict(self, X, y=None):
+        
+        dt_transformed = pd.merge(X, self.dt_states, how='left')
+        dt_transformed.fillna(-1, inplace=True)
+        
+        return dt_transformed["state"].astype(int).as_matrix()
+    
+    
+    def fit_predict(self, X, y=None):
+        
+        self.fit(X)
+        return self.predict(X)
---
+++ transformers/BooleanTransformer.py
@@ -0,0 +1,57 @@
+from sklearn.base import TransformerMixin
+import pandas as pd
+import numpy as np
+from time import time
+import sys
+
+class BooleanTransformer(TransformerMixin):
+    
+    def __init__(self, case_id_col, cat_cols, num_cols, fillna=True):
+        self.case_id_col = case_id_col
+        self.cat_cols = cat_cols
+        self.num_cols = num_cols
+        self.fillna = fillna
+        
+        self.columns = None
+        
+        self.fit_time = 0
+        self.transform_time = 0
+    
+    
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X, y=None):
+        start = time()
+        
+        # transform numeric cols
+        if len(self.num_cols) > 0:
+            dt_numeric = X.groupby(self.case_id_col)[self.num_cols].agg({'mean':np.mean, 'max':np.max, 'min':np.min, 'sum':np.sum, 'std':np.std})
+            dt_numeric.columns = ['_'.join(col).strip() for col in dt_numeric.columns.values]
+            
+        # transform cat cols
+        dt_transformed = pd.get_dummies(X[self.cat_cols])
+        dt_transformed[self.case_id_col] = X[self.case_id_col]
+        del X
+        dt_transformed = dt_transformed.groupby(self.case_id_col).max() # boolean
+        
+        # concatenate
+        if len(self.num_cols) > 0:
+            dt_transformed = pd.concat([dt_transformed, dt_numeric], axis=1)
+            del dt_numeric
+        
+        # fill missing values with 0-s
+        if self.fillna:
+            dt_transformed.fillna(0, inplace=True)
+            
+        # add missing columns if necessary
+        if self.columns is None:
+            self.columns = dt_transformed.columns
+        else:
+            missing_cols = [col for col in self.columns if col not in dt_transformed.columns]
+            for col in missing_cols:
+                dt_transformed[col] = 0
+            dt_transformed = dt_transformed[self.columns]
+        
+        self.transform_time = time() - start
+        return dt_transformed
