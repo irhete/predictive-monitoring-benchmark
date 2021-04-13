ec7034d7c1ec7466493118919b3c9182c35861f1
Manuel Camargo <manuel.camargo@ut.ee>
2021-04-13 14:02:58 +0300

    Update IndexBasedTransformer.py

--- transformers/IndexBasedTransformer.py
+++ transformers/IndexBasedTransformer.py
@@ -4,7 +4,7 @@ import numpy as np
 from time import time
 
 class IndexBasedTransformer(TransformerMixin):
-    
+
     def __init__(self, case_id_col, cat_cols, num_cols, max_events=None, fillna=True, create_dummies=True):
         self.case_id_col = case_id_col
         self.cat_cols = cat_cols
@@ -12,37 +12,35 @@ class IndexBasedTransformer(TransformerMixin):
         self.max_events = max_events
         self.fillna = fillna
         self.create_dummies = create_dummies
-        
+
         self.columns = None
-        
+
         self.fit_time = 0
         self.transform_time = 0
-    
-    
+
+
     def fit(self, X, y=None):
         return self
-    
+
     def transform(self, X, y=None):
         start = time()
-        
+
         grouped = X.groupby(self.case_id_col, as_index=False)
-        
         if self.max_events is None:
-            self.max_events = grouped.size().max(numeric_only=True).size
-        
-        
+            self.max_events = grouped.size().max(numeric_only=True).reset_index().to_dict('records')[0][0]
+
         dt_transformed = pd.DataFrame(grouped.apply(lambda x: x.name), columns=[self.case_id_col])
         for i in range(0, self.max_events):
             dt_index = grouped.nth(i)[[self.case_id_col] + self.cat_cols + self.num_cols]
             dt_index.columns = [self.case_id_col] + ["%s_%s"%(col, i) for col in self.cat_cols] + ["%s_%s"%(col, i) for col in self.num_cols]
             dt_transformed = pd.merge(dt_transformed, dt_index, on=self.case_id_col, how="left")
         dt_transformed.index = dt_transformed[self.case_id_col]
-        
+
         # one-hot-encode cat cols
         if self.create_dummies:
             all_cat_cols = ["%s_%s"%(col, i) for col in self.cat_cols for i in range(self.max_events)]
             dt_transformed = pd.get_dummies(dt_transformed, columns=all_cat_cols).drop(self.case_id_col, axis=1)
-        
+
         # fill missing values with 0-s
         if self.fillna:
             dt_transformed = dt_transformed.fillna(0)
@@ -58,6 +56,6 @@ class IndexBasedTransformer(TransformerMixin):
 
         self.transform_time = time() - start
         return dt_transformed
-    
+
     def get_feature_names(self):
         return self.columns
